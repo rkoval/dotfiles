0bb7964e04ce0c07d082c922a6178f24a0be88c4
Ryan Koval <ryan@ryankoval.com>
2019-05-13 17:37:14 -0500

    added more macos updates

--- init/macos.sh
+++ init/macos.sh
@@ -1,2 +1,8 @@
 #!/usr/bin/env bash
+
+# to fix key repeating in sublime
 defaults write com.sublimetext.3 ApplePressAndHoldEnabled -bool false
+
+# to make fonts less blurry in external monitors
+defaults write -g CGFontRenderingFontSmoothingDisabled -bool NO
+defaults -currentHost write -globalDomain AppleFontSmoothing -int 3
