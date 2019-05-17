8e373c769cddc565a1790c91b8a70c8a0cedecb1
Ryan Koval <ryan@ryankoval.com>
2019-05-17 15:41:24 -0500

    added more macos preferences

--- init/macos.sh
+++ init/macos.sh
@@ -6,3 +6,19 @@ defaults write -g ApplePressAndHoldEnabled -bool false
 # to make fonts less blurry in external monitors
 defaults write -g CGFontRenderingFontSmoothingDisabled -bool NO
 defaults -currentHost write -globalDomain AppleFontSmoothing -int 3
+
+# show scrollbars always
+defaults write -g AppleShowScrollBars -string Always
+
+# dark mode
+defaults write -g AppleInterfaceStyle -string Dark
+
+# key repeat
+defaults write -g InitialKeyRepeat -int 15
+defaults write -g KeyRepeat -int 2
+
+# disable force click
+defaults write -g "com.apple.trackpad.forceClick" -int 0;
+
+# natural scroll direction is unnatural
+defaults write -g "com.apple.swipescrolldirection" -int 0
