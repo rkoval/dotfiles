307bf7302d8c9d54ccf4443c195966509c0eb863
Ryan Koval <ryan@ryankoval.com>
2022-05-16 00:17:40 -0500

    fixed friendlysnippets

--- nvim/lua/ryankoval/luasnip.lua
+++ nvim/lua/ryankoval/luasnip.lua
@@ -5,6 +5,7 @@ end
 local luasnip = require('luasnip')
 
 require('luasnip.loaders.from_lua').load({ paths = '~/dotfiles/nvim/lua/ryankoval/snippets' })
+require('luasnip.loaders.from_vscode').load({ paths = '~/dotfiles/nvim/dein/repos/github.com/rafamadriz/friendly-snippets' })
 
 luasnip.filetype_extend('typescript', { 'javascript' })
 luasnip.filetype_extend('typescriptreact', { 'javascript' })
--- nvim/lua/ryankoval/snippets/lua/foreach.lua
+++
@@ -1,8 +0,0 @@
-return {
-  parse(
-    'foreach',
-    [[for ${1:key},${2:value} in pairs(${3:table}) do
-    ${0:-- do something}
-end]]
-  ),
-}
