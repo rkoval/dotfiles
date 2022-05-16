785cf167ec3615108a70f026eae66b8318ac24e5
Ryan Koval <ryan@ryankoval.com>
2022-05-16 02:53:07 -0500

    fixed missing arg

--- nvim/lua/ryankoval/lsp.lua
+++ nvim/lua/ryankoval/lsp.lua
@@ -173,7 +173,7 @@ lspconfig.efm.setup({
   },
 
   handlers = {
-    ['textDocument/publishDiagnostics'] = vim.lsp.with(vim.lsp.diagnostic.on_publish_diagnostics),
+    ['textDocument/publishDiagnostics'] = vim.lsp.with(vim.lsp.diagnostic.on_publish_diagnostics, {}),
   },
 })
 
