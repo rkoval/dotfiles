7ddffdb2d86275d3bd663d258c88169089091d79
Ryan Koval <ryan@ryankoval.com>
2022-05-16 00:07:28 -0500

    tweaked fugitive hotkeys

---
+++ nvim/after/ftplugin/fugitiveblame.lua
@@ -0,0 +1,10 @@
+vim.keymap.set('n', '<esc>', ':q<cr>', {
+  noremap = true,
+  silent = true,
+  buffer = true,
+})
+vim.keymap.set('n', 'q', ':q<cr>', {
+  noremap = true,
+  silent = true,
+  buffer = true,
+})
