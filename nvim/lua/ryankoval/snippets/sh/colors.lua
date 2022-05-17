749b5903d3c4a4725e52aa1fe750beabd517c08c
Ryan Koval <ryan@ryankoval.com>
2022-05-17 01:26:28 -0500

    cleaned up snippets

---
+++ nvim/lua/ryankoval/snippets/sh/colors.lua
@@ -0,0 +1,35 @@
+return {
+  parse('resetcolor', "RESET_COLOR='\033[0m'"),
+
+  parse('black', "BLACK='\033[0;30m'"),
+
+  parse('red', "RED='\033[0;31m'"),
+
+  parse('green', "GREEN='\033[0;32m'"),
+
+  parse('brown', "BROWN='\033[0;33m'"),
+
+  parse('blue', "BLUE='\033[0;34m'"),
+
+  parse('purple', "PURPLE='\033[0;35m'"),
+
+  parse('cyan', "CYAN='\033[0;36m'"),
+
+  parse('light_gray', "LIGHT_GRAY='\033[0;37m'"),
+
+  parse('gray', "GRAY='\033[1;30m'"),
+
+  parse('light_red', "LIGHT_RED='\033[1;31m'"),
+
+  parse('light_green', "LIGHT_GREEN='\033[1;32m'"),
+
+  parse('yellow', "YELLOW='\033[1;33m'"),
+
+  parse('light_blue', "LIGHT_BLUE='\033[1;34m'"),
+
+  parse('light_purple', "LIGHT_PURPLE='\033[1;35m'"),
+
+  parse('light_cyan', "LIGHT_CYAN='\033[1;36m'"),
+
+  parse('white', "WHITE='\033[1;37m'"),
+}
