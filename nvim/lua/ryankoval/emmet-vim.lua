e838cfde9a12d0555204e950c4fdcd714249f00e
Ryan Koval <ryan@ryankoval.com>
2022-05-19 03:13:20 -0500

    fixed emmet

--- nvim/lua/ryankoval/emmet-vim.lua
+++ nvim/lua/ryankoval/emmet-vim.lua
@@ -4,5 +4,5 @@ vim.g.user_emmet_install_command = 0
 vim.g.user_emmet_mode = 'iv' -- enable emmet functions for `i`nsert and `v`isual
 
 vim.cmd([[
-  autocmd MyAutoCmd FileType html,css,vue EmmetInstall | imap <buffer> <C-Return> <Plug>(emmet-expand-abbr)
+  autocmd FileType html,css,vue EmmetInstall | imap <buffer> <C-Return> <Plug>(emmet-expand-abbr)
 ]])
