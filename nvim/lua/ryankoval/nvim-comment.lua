c88bb92bb64b6e9b9b415f5f51b72c6e94a0ef0c
Ryan Koval <ryan@ryankoval.com>
2022-05-18 21:12:24 -0500

    fixed nvim comment

--- nvim/lua/ryankoval/nvim-comment.lua
+++ nvim/lua/ryankoval/nvim-comment.lua
@@ -1,6 +1,6 @@
 require('nvim_comment').setup({ create_mappings = false })
 
 local opts = { silent = true }
-vim.keymap.set('n', '<D-/>', ':CommentToggle<cr><Up>', opts)
+vim.keymap.set('n', '<D-/>', ':CommentToggle<cr>', opts)
 vim.keymap.set('v', '<D-/>', ':CommentToggle<cr>', opts)
-vim.keymap.set('i', '<D-/>', ':CommentToggle<cr>', opts)
+vim.keymap.set('i', '<D-/>', '<esc>:CommentToggle<cr>a', opts)
