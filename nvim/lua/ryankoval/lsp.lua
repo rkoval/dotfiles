ed079db91558f1661d0dc557e1940576203711a8
Ryan Koval <ryan@ryankoval.com>
2022-05-19 03:08:51 -0500

    configured docker/docker-compose

--- nvim/lua/ryankoval/lsp.lua
+++ nvim/lua/ryankoval/lsp.lua
@@ -75,7 +75,7 @@ vim.diagnostic.config({ underline = true, virtual_text = false, update_in_insert
 --------------------------------------------------------------------------------
 
 require('nvim-lsp-installer').setup({
-  ensure_installed = { 'sumneko_lua', 'eslint', 'tsserver', 'bashls', 'pyright', 'vimls', 'gopls', 'golangci_lint_ls', 'yamlls' },
+  ensure_installed = { 'sumneko_lua', 'eslint', 'tsserver', 'bashls', 'pyright', 'vimls', 'gopls', 'golangci_lint_ls', 'yamlls', 'dockerls' },
   automatic_installation = true, -- automatically detect which servers to install (based on which servers are set up via lspconfig)
 })
 local capabilities = require('cmp_nvim_lsp').update_capabilities(vim.lsp.protocol.make_client_capabilities())
@@ -231,6 +231,7 @@ require('lspconfig').yamlls.setup({
     yaml = {
       schemas = {
         ['https://json.schemastore.org/github-workflow.json'] = '/.github/workflows/*',
+        ['https://raw.githubusercontent.com/compose-spec/compose-spec/master/schema/compose-spec.json'] = 'docker-compose.yml',
       },
     },
   },
@@ -265,3 +266,6 @@ vim.api.nvim_create_autocmd('BufWritePre', {
     end
   end,
 })
+
+-- docker ---------------------------------------------------------------------
+lspconfig.dockerls.setup({})
