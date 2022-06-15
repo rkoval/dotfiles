f8674d327435c21a6bffc21ce32ce45644595083
Ryan Koval <ryan@ryankoval.com>
2022-06-14 20:49:12 -0500

    added css lsp support

--- nvim/lua/ryankoval/lsp.lua
+++ nvim/lua/ryankoval/lsp.lua
@@ -75,7 +75,19 @@ vim.diagnostic.config({ underline = true, virtual_text = false, update_in_insert
 --------------------------------------------------------------------------------
 
 require('nvim-lsp-installer').setup({
-  ensure_installed = { 'sumneko_lua', 'eslint', 'tsserver', 'bashls', 'pyright', 'vimls', 'gopls', 'golangci_lint_ls', 'yamlls', 'dockerls' },
+  ensure_installed = {
+    'sumneko_lua',
+    'eslint',
+    'tsserver',
+    'bashls',
+    'pyright',
+    'vimls',
+    'gopls',
+    'golangci_lint_ls',
+    'yamlls',
+    'dockerls',
+    'cssls',
+  },
   automatic_installation = true, -- automatically detect which servers to install (based on which servers are set up via lspconfig)
 })
 local capabilities = require('cmp_nvim_lsp').update_capabilities(vim.lsp.protocol.make_client_capabilities())
@@ -271,3 +283,6 @@ vim.api.nvim_create_autocmd('BufWritePre', {
 
 -- docker ---------------------------------------------------------------------
 lspconfig.dockerls.setup({})
+
+-- less ---------------------------------------------------------------------
+lspconfig.cssls.setup({})
