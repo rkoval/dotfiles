462e805e2f5e1ce58906cc6fdf80d3dde5c9f5b7
Ryan Koval <ryan@ryankoval.com>
2022-06-14 20:52:53 -0500

    fixed emmet support for less

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -135,7 +135,7 @@ return require('packer').startup(function()
   })
   use({
     'mattn/emmet-vim',
-    ft = { 'html', 'css', 'vue' },
+    ft = { 'html', 'css', 'less', 'scss', 'vue' },
     config = function()
       require('ryankoval.emmet-vim')
     end,
--- nvim/lua/ryankoval/emmet-vim.lua
+++ nvim/lua/ryankoval/emmet-vim.lua
@@ -1,8 +1,10 @@
-vim.g.use_emmet_complete_tag = 0
+vim.g.user_emmet_complete_tag = 0
 vim.g.user_emmet_install_global = 0
 vim.g.user_emmet_install_command = 0
 vim.g.user_emmet_mode = 'iv' -- enable emmet functions for `i`nsert and `v`isual
 
 vim.cmd([[
-  autocmd FileType html,css,vue EmmetInstall | imap <buffer> <C-Return> <Plug>(emmet-expand-abbr)
+  autocmd FileType html,css,less,scss,vue EmmetInstall
 ]])
+
+vim.keymap.set('i', '<C-Return>', '<Plug>(emmet-expand-abbr)', { noremap = true, silent = true })
