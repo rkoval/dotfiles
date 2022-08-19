f706be4d9ff4e83974f020c44c33a5346a4516a9
Ryan Koval <ryan@ryankoval.com>
2022-08-18 21:20:51 -0500

    fixed friendlysnippets

--- nvim/lua/friendly-snippets/init.lua
+++ nvim/lua/friendly-snippets/init.lua
@@ -3,4 +3,4 @@ if not pcall(require, 'cmp') then
 end
 
 -- we want this file to be separate from our other snippets so that this doesn't get reloaded every time we :Src
-require('luasnip.loaders.from_vscode').load({ paths = '~/dotfiles/nvim/dein/repos/github.com/rafamadriz/friendly-snippets' })
+require('luasnip.loaders.from_vscode').load({ paths = '~/.local/share/nvim/site/pack/packer/start/friendly-snippets' })
