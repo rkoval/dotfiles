f9c9810cad49da32f2fe2dd4968141d37239d6fa
Ryan Koval <ryan@ryankoval.com>
2022-08-22 22:30:40 -0500

    added visual mappings

--- nvim/lua/ryankoval/vim-visual-multi.lua
+++ nvim/lua/ryankoval/vim-visual-multi.lua
@@ -1,3 +1,7 @@
+-- vim.g.VM_maps = {}
+
 local opts = { noremap = true, silent = true }
 vim.keymap.set('n', '<D-d>', '<Plug>(VM-Find-Under)', opts)
+vim.keymap.set('v', '<D-d>', '<Plug>(VM-Visual-Add)', opts)
 vim.keymap.set('n', '<D-S-d>', '<Plug>(VM-Select-All)', opts)
+vim.keymap.set('v', '<D-S-d>', '<Plug>(VM-Visual-All)', opts)
