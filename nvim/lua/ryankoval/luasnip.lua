f8005c35322c4b8e557a87c16fa6255e49cc78a5
Ryan Koval <ryan@ryankoval.com>
2022-08-25 15:35:10 -0500

    added lazy load of snippets

--- nvim/lua/ryankoval/luasnip.lua
+++ nvim/lua/ryankoval/luasnip.lua
@@ -4,7 +4,7 @@ end
 
 local luasnip = require('luasnip')
 
-require('luasnip.loaders.from_lua').load({ paths = '~/dotfiles/nvim/lua/ryankoval/snippets' })
+require('luasnip.loaders.from_lua').lazy_load({ paths = '~/dotfiles/nvim/lua/ryankoval/snippets' })
 
 luasnip.filetype_extend('typescript', { 'javascript' })
 luasnip.filetype_extend('typescriptreact', { 'javascript' })
