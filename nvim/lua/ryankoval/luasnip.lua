f0891e0c09ddf5a772058b8761fe079b41bdb51e
Ryan Koval <ryan@ryankoval.com>
2022-09-07 13:37:06 -0500

    fixed luasnip expanding snippets after normal mode exit

--- nvim/lua/ryankoval/luasnip.lua
+++ nvim/lua/ryankoval/luasnip.lua
@@ -6,6 +6,21 @@ local luasnip = require('luasnip')
 
 require('luasnip.loaders.from_lua').lazy_load({ paths = '~/dotfiles/nvim/lua/ryankoval/snippets' })
 
+local function leave_snippet()
+  if
+    ((vim.v.event.old_mode == 's' and vim.v.event.new_mode == 'n') or vim.v.event.old_mode == 'i')
+    and luasnip.session.current_nodes[vim.api.nvim_get_current_buf()]
+    and not luasnip.session.jump_active
+  then
+    luasnip.unlink_current()
+  end
+end
+
+-- stop snippets when you leave to normal mode
+vim.api.nvim_create_autocmd('ModeChanged', {
+  callback = leave_snippet,
+})
+
 luasnip.filetype_extend('typescript', { 'javascript' })
 luasnip.filetype_extend('typescriptreact', { 'javascript' })
 luasnip.filetype_extend('typescriptreact', { 'javascriptreact' })
