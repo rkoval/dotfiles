3a3ae1a1eefc35ce474c66eaefc42831d458efe0
Ryan Koval <ryan@ryankoval.com>
2022-11-01 17:46:56 -0500

    fixed lsp

--- nvim/lua/ryankoval/lsp.lua
+++ nvim/lua/ryankoval/lsp.lua
@@ -98,7 +98,7 @@ require('nvim-lsp-installer').setup({
   },
   automatic_installation = true, -- automatically detect which servers to install (based on which servers are set up via lspconfig)
 })
-local capabilities = require('cmp_nvim_lsp').update_capabilities(vim.lsp.protocol.make_client_capabilities())
+local capabilities = require('cmp_nvim_lsp').default_capabilities(vim.lsp.protocol.make_client_capabilities())
 local lspconfig = require('lspconfig')
 
 -- TSSERVER --------------------------------------------------------------------
