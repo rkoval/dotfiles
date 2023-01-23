82054c99db7b59d4ddc8da54c9ed6057a8224d26
Ryan Koval <ryan@ryankoval.com>
2023-01-23 17:59:45 -0600

    removed ss alias

--- source/50_osx.sh
+++ source/50_osx.sh
@@ -5,9 +5,6 @@ is_osx || return 1
 # Trim new lines and copy to clipboard
 alias c="tr -d '\n' | pbcopy"
 
-# Start ScreenSaver. This will lock the screen if locking is enabled.
-alias ss="open /System/Library/Frameworks/ScreenSaver.framework/Versions/A/Resources/ScreenSaverEngine.app"
-
 # open current window in path finder
 alias pf='open -a "Path Finder.app" .'
 
