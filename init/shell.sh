4c9928885d3c54293aefd1c9477db24775f9427a
Ryan Koval <ryan@ryankoval.com>
2023-06-26 19:36:37 -0500

    fixed shell

--- init/shell.sh
+++ init/shell.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 # Make ZSH default
-if [[ "$SHELL" != "/usr/local/bin/zsh" ]]; then
+if [[ "$SHELL" != "/bin/zsh" ]]; then
   echo "Making /bin/zsh your default shell"
-  sudo chsh -s "/usr/local/bin/zsh" "$USER" >/dev/null 2>&1
+  sudo chsh -s "/bin/zsh" "$USER" >/dev/null 2>&1
   echo "Please exit and restart all your shells."
 fi
