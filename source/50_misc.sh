58e028394ca35f0b4f0ab4078ba4e5e454e8aa7f
Ryan Koval <ryan@ryankoval.com>
2023-10-04 11:23:55 -0500

    cleaned

--- source/50_misc.sh
+++ source/50_misc.sh
@@ -46,8 +46,11 @@ gbu() {
   fi
 }
 
-gbcleanup() {
+gbclean() {
   git fetch -p && for branch in $(git for-each-ref --format '%(refname) %(upstream:track)' refs/heads | awk '$2 == "[gone]" {sub("refs/heads/", "", $1); print $1}'); do git branch -D $branch; done
+}
+gcomclean() {
+  gcom && gfr && gbclean
 }
 
 gptags () {
