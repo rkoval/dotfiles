53e44118c1831fee334eefb82a5a9cf1c033d5bf
Ryan Koval <ryan@ryankoval.com>
2025-10-17 11:07:03 -0500

    added sync config

--- link-work-sync-settings.sh
+++ link-work-sync-settings.sh
@@ -13,3 +13,6 @@ echo "you must manually reload config in linearmouse General settings for this t
 rm -f "$HOME/Library/Application Support/com.mitchellh.ghostty/config"
 rm -rf "$HOME/.config/ghostty"
 ln -sFv "$WORK_SYNC_DIR/ghostty" ~/.config/ghostty
+
+ln -sFv "$WORK_SYNC_DIR/Cursor/settings.json" "$HOME/Library/Application\ Support/Cursor/User/settings.json"
+ln -sFv "$WORK_SYNC_DIR/Cursor/keybindings.json" "$HOME/Library/Application\ Support/Cursor/User/keybindings.json"
